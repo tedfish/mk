f3a3d7a2e22ac7a3b76004de9be5b2a5d8173e98
TDN <tedfish@gmail.com>
2021-07-27 16:44:23 -0700

    try

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import Layout from '../components/layout'
 import Search from '../components/search'
 import Features from '../components/features'
 import Seo from '../components/seo'
-import firebase from "../components/firebase"
-import ItemList from "../components/itemlist"
-import AddItemForm from '../components/additemform'
-import UpdateItem from "../components/updateitem";
+// import firebase from "../components/firebase"
+// import ItemList from "../components/itemlist"
+// import AddItemForm from '../components/additemform'
+// import UpdateItem from "../components/updateitem";
 import Container from '@material-ui/core/Container';
 // import { makeStyles } from '@material-ui/core/styles'
 const IndexPage = () => {
-  const initialItemState = [
-    { id: null, name: "", type: "", qty: "", description: "" }
-  ];
-  const [editing, setEditing] = useState(false);
-  const [currentItem, setCurrentItem] = useState(initialItemState);
-  const editItem = item => {
-    setEditing(true);
-    setCurrentItem({
-      id: item.id,
-      name: item.name,
-      type: item.type,
-      qty: item.qty,
-      description: item.description
-    });
-  };
-  const updateItem = ({ currentItem }, updatedItem) => {
-    console.log(
-      "It send the item to the updated item function:",
-      updatedItem,
-      currentItem.id
-    );
-    setEditing(false);
-    firebase
-      .firestore()
-      .collection("items")
-      .doc(currentItem.id)
-      .update(updatedItem);
-  };
+  // const initialItemState = [
+  //   { id: null, name: "", type: "", qty: "", description: "" }
+  // ];
+  // const [editing, setEditing] = useState(false);
+  // const [currentItem, setCurrentItem] = useState(initialItemState);
+  // const editItem = item => {
+  //   setEditing(true);
+  //   setCurrentItem({
+  //     id: item.id,
+  //     name: item.name,
+  //     type: item.type,
+  //     qty: item.qty,
+  //     description: item.description
+  //   });
+  // };
+  // const updateItem = ({ currentItem }, updatedItem) => {
+  //   console.log(
+  //     "It send the item to the updated item function:",
+  //     updatedItem,
+  //     currentItem.id
+  //   );
+  //   setEditing(false);
+  //   firebase
+  //     .firestore()
+  //     .collection("items")
+  //     .doc(currentItem.id)
+  //     .update(updatedItem);
+  // };
   // firebase
   //   .firestore()
   //   .collection("items")
@@ -57,7 +57,7 @@ const IndexPage = () => {
       <Search />
       <Features />
       <Container>
-      <ItemList editItem={editItem} />
+      {/* <ItemList editItem={editItem} />
       <h2>Add Item</h2>
       {editing ? (
         <UpdateItem
@@ -67,7 +67,7 @@ const IndexPage = () => {
         />
       ) : (
         <AddItemForm />
-      )}
+      )} */}
       </Container>
     </Layout>
   )
